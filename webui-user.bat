b7f3cd2f322436300e395a4ac2f39224748673c4
boombbo <chrisbbo812@gmail.com>
2024-04-16 06:46:41 +0000

    Apr 16, 2024, 2:46 PM

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,12 @@
 @echo off
 
+set TRANSFORMERS_OFFLINE=1
+set TF_ENABLE_ONEDNN_OPTS=0
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--no-download-sd-model --xformers   --api --autolaunch  --enable-insecure-extension-access --disable-safe-unpickle   --skip-version-check
+
+git pull
 
 call webui.bat
